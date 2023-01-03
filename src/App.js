ee1061d6aa3df09a8028557dee680f185eaf2388
Raga <ragawilliam570@gmail.com>
2023-01-03 17:10:10 +0300

    Add main App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+
+import { Route, Routes } from 'react-router-dom';
+
+import Greeting from './components/Greeting';
+import Home from './components/Home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/greeting" element={<Greeting/>}/>
+      </Routes>
     </div>
   );
 }
