0a19843542cd1f46e55d0fbefc6e785c95a20e6a
Raga <ragawilliam570@gmail.com>
2023-01-03 17:10:37 +0300

    Add Greeting component

---
+++ src/components/Greeting.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { fetchRandomMessage } from '../redux/messagesReducer';
+
+const Greeting = () => {
+  const state = useSelector((state) => state)
+
+  const { message } = state
+
+  // console.log(message)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRandomMessage());
+  }, [dispatch])
+  
+  return (
+    <>
+      <p>Greetings {message}!</p>
+    </>
+  )
+}
+
+export default Greeting;
